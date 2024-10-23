ceb2b7045240f031e920a5afd3ed3a9ec987e6b6
nvt1802 <tainguyen6600@gmail.com>
2024-10-23 21:12:01 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import HeroBanner from "@/src/components/HeroBanner";
 import LatestPost from "@/src/components/LatestPost";
+import { IPost } from "@/src/types/posts";
 import { fetchPosts } from "@/src/utils/api/posts";
 import { Metadata } from "next";
 
@@ -7,14 +8,25 @@ export const metadata: Metadata = {
   title: "Home Page - Daily Curiosity",
 };
 
+export const revalidate = 60;
+export const dynamicParams = true;
+
 const Home = async () => {
-  const { items } = await fetchPosts(1);
+  let posts: IPost[] = [];
+
+  try {
+    const { items } = await fetchPosts(1);
+    posts = items;
+  } catch (error) {
+    console.error(error);
+  }
+
   return (
     <>
       <div className="flex flex-col gap-5">
         <HeroBanner />
         <div className="mx-4">
-          <LatestPost posts={items} />
+          <LatestPost posts={posts} />
         </div>
       </div>
     </>
--- src/utils/api/posts.ts
+++ src/utils/api/posts.ts
@@ -3,7 +3,7 @@ import axiosInstance from "../axiosInstance";
 
 export const fetchPosts = async (page: number) => {
   const { data } = await axiosInstance.get<IPostResponse>(
-    `/api/external/posts?page=${page}&limit=9`
+    `${process.env.NEXT_PUBLIC_CMS_API_URL}api/external/posts?page=${page}&limit=9`
   );
   return data.data;
 };
